9c7343d3a4a2cbc3dd5ac01ab957e6f5ed645bbf
Samuel Espinho-Alves <samuelespinho@gmail.com>
2021-08-27 22:07:00 +0100

    make pages responsive and prepare for parallax

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import "./App.css";
 import PageOne from "./components/Home/PageOne";
 import PageTwo from "./components/Home/PageTwo";
+import PageThree from "./components/Home/PageThree";
 
 function App() {
   return (
     <div className="App">
       <PageOne />
       <PageTwo />
+      <PageThree />
     </div>
   );
 }
---
+++ src/components/Home/PageThree.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import "./PageThree.css";
+
+
+const PageOne = () => {
+  return (
+    <div className="PageThree">
+      Hi from PageThree!
+    </div>
+    );
+}
+ 
+export default PageOne;
