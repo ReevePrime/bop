14411c5873d4c2fcb1de420c9eac2bb322f377a1
Samuel Espinho-Alves <samuelespinho@gmail.com>
2021-08-30 15:53:25 +0100

    add fadein for cards on main page

--- src/components/Home/PageThree.js
+++ src/components/Home/PageThree.js
@@ -1,15 +1,31 @@
 import React from 'react';
 import "./PageThree.css";
 import PageThreeCard from './PageThreeCard';
+import Fade from 'react-reveal/Fade';
+import bags from "../images/bags.jpg";
+import paynebs from "../images/paynebs.jpg";
+import isme from "../images/isme.jpg";
 
 
 const PageOne = () => {
   return (
     <div className="PageThree">
       <div className="Card-container">
-        <PageThreeCard className="p3-card card1" title={"My coffees! (buy them)"} />
-        <PageThreeCard className="p3-card card2" title={"My store!"} />
-        <PageThreeCard className="p3-card card3" title={"About me"} />
+        <Fade left>
+          <div className="p3-card card1">
+          <PageThreeCard title={"My coffees!"} imgSrc={bags}/>
+          </div>
+        </Fade>
+        <Fade right>
+          <div className="p3-card card2" >
+          <PageThreeCard title={"My store! (buy all my coffees)"} imgSrc={paynebs} />
+          </div>
+        </Fade>
+        <Fade left>
+          <div className="p3-card card3">
+        <PageThreeCard title={"About me"} imgSrc={isme} />
+        </div>
+        </Fade>
       </div>
     </div>
     );
