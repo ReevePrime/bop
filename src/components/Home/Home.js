1c96acd76be9216b99830ca8b7b047d1aed64a30
Samuel Espinho-Alves <samuelespinho@gmail.com>
2021-09-08 20:50:32 +0100

    add fourth section of home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React from "react";
 import PageOne from "./PageOne";
 import PageTwo from "./PageTwo";
 import PageThree from "./PageThree";
+import PageFour from "./PageFour";
 
 function Home() {
   return (
@@ -9,6 +10,7 @@ function Home() {
       <PageOne />
       <PageTwo />
       <PageThree />
+      <PageFour />
     </div>
   );
 }
---
+++ src/components/Home/PageFour.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PageFour = () => {
+  return (
+    <React.Fragment>
+      Hi!
+    </React.Fragment>
+    );
+}
+ 
+export default PageFour;
